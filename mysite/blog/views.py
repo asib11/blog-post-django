dbd3b9cb6567bd973a049cbd3697772988652d5c
asib ahmed <ahmedasib1@gmail.com>
2024-06-15 09:40:51 +0000

    feat: load dynamic data in templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'name': 'Asib',
+        'occupation': 'student',
+        'subject': 'CSE',
+        'id': 18191103031,
+        'section': 1,
+        'intake': 40
+    },
+    {
+        'name': 'Tanveer',
+        'occupation': 'student',
+        'subject': 'CSE',
+        'id': 18191103037,
+        'section': 1,
+        'intake': 40
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {'posts': posts}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
