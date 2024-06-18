15078285f409add23fafa3523a524f157e90bdd7
asib <ahmedasib1@gmail.com>
2024-06-18 23:55:14 +0600

    feat: make database model and use migrations

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models # type: ignore
+from django.utils import timezone # type: ignore
+from django.contrib.auth.models import User # type: ignore
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    post_time = models.DateTimeField(default=timezone.now)
+    auth = models.ForeignKey(User, on_delete=models.CASCADE)
+    
