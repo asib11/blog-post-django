e939728d0699038b956cea8f55679fa1c9af6b58
asib <ahmedasib1@gmail.com>
2024-06-21 11:50:05 +0600

    feat: set admin for update post

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -8,4 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     post_time = models.DateTimeField(default=timezone.now)
     auth = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
+
+    def __str__(self):
+        return self.title
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Post
-
-posts = [
-    {
-        'author': 'Asib Ahmed',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2024'
-    },
-    {
-        'author': 'Tanveer Tayeb',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2024'
-    }
-]
 
 # Create your views here.
 def home(request):
