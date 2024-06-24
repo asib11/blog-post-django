13a8808aa80ed6ccb83ce9a77e61af1ac428eda9
asib <ahmedasib1@gmail.com>
2024-06-24 18:52:28 +0600

    feat: form creation and add email field

---
+++ mysite/user/froms.py
@@ -0,0 +1,10 @@
+from django import forms # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from django.contrib.auth.forms import UserCreationForm # type: ignore
+
+class UserRegistrationFrom(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- mysite/user/views.py
+++ mysite/user/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect # type: ignore
 from django.contrib.auth.forms import UserCreationForm # type: ignore
 from django.contrib import messages # type: ignore
+from .froms import UserRegistrationFrom
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationFrom(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!!!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationFrom()
     return render(request, 'user/register.html', {'form':form})
