8696d47a149f7493504467a411d0cc138f2eca96
asib <ahmedasib1@gmail.com>
2024-06-24 23:00:13 +0600

    feat: crispy from use

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig', # efficient,
     'user.apps.UserConfig',
     # 'user'
+    'crispy_forms',
+    'crispy_bootstrap5', 
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
