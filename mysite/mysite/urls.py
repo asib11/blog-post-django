aadd95bdb89d4be39da7b89518628cc31814f1d7
asib <ahmedasib1@gmail.com>
2024-06-25 21:59:30 +0600

    feat: login user redirect

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_view.register, name='register'),
     path('login/', auth_view.LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('logout/', auth_view.LogoutView.as_view(), name='logout'),
     path('', include('blog.urls')),
 ]
