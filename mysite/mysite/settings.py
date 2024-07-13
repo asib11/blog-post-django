1568ea30b06412a149df83fde0e6616eca2845b5
asib <ahmedasib1@gmail.com>
2024-07-13 23:31:25 +0600

    feat: update image and resize the images

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEIDA_URL = '/media'
+MEIDA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
